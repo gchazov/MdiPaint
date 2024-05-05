ad1b800ea3f73495b1d9905839a911cbbff2a503
Your Name gchazov <cheyzov@outlook.com>
2024-05-05 15:53:03 +0500

    new structure

---
+++ ReversePlugin/ReverseTransform.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PluginInterface;
+using System.Drawing;
+
+namespace ReversePlugin
+{
+    [Version(1, 0)]
+    public class ReverseTransform : IPlugin
+    {
+        public string Name
+        {
+            get
+            {
+                return "Переворот изображения";
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return "Me";
+            }
+        }
+
+        public void Transform(Bitmap bitmap)
+        {
+            for (int i = 0; i < bitmap.Width; ++i)
+                for (int j = 0; j < bitmap.Height / 2; ++j)
+                {
+                    Color color = bitmap.GetPixel(i, j);
+                    bitmap.SetPixel(i, j, bitmap.GetPixel(i, bitmap.Height - j - 1));
+                    bitmap.SetPixel(i, bitmap.Height - j - 1, color);
+                }
+        }
+    }
+
+}
